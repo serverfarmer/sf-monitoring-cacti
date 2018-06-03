cfbbd71f4104fac2c556ba4f30790ca982cc0feb
Tomasz Klim <github@tomaszklim.pl>
2018-06-04 00:09:31 +0200

    removed caching of usb drives info

--- cron/smart-helper.sh
+++ cron/smart-helper.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
 path="/var/cache/cacti"
-
-if [ ! -f $path/usb.tmp ]; then
-	touch $path/usb.tmp
-fi
-
-devices=`/opt/farm/ext/standby-monitor/utils/list-physical-drives.sh |grep -vxFf /etc/local/.config/standby.exceptions`
+devices=`/opt/farm/ext/standby-monitor/utils/list-physical-drives.sh |grep -vxFf /etc/local/.config/skip-smart.devices`
 
 for device in $devices; do
-	devname=`readlink -f $device`
+	base="`basename $device`"
+	file="$path/$base.txt"
 
-	if ! grep -qxF $devname $path/usb.tmp || [ "$1" = "--force" ]; then
-		base="`basename $device`"
-		file="$path/$base.txt"
+	/usr/sbin/smartctl -d sat -T permissive -a $device >$file.new
+	mv -f $file.new $file 2>/dev/null
 
-		/usr/sbin/smartctl -d sat -T permissive -a $device >$file.new
-		mv -f $file.new $file 2>/dev/null
-
-		/opt/farm/ext/monitoring-cacti/cron/send.sh $file
-	fi
+	/opt/farm/ext/monitoring-cacti/cron/send.sh $file
 done
