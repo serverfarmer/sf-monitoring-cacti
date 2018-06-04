6a25645005d6f79365ba63c6cfd8c11c23e6e1b5
Tomasz Klim <github@tomaszklim.pl>
2018-06-04 23:05:31 +0200

    smart monitoring moved to separate sf-monitoring-smart extension

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@
 
 /opt/farm/scripts/setup/extension.sh sf-standby-monitor
 
+if [ "$HWTYPE" = "physical" ]; then
+	/opt/farm/scripts/setup/extension.sh sf-monitoring-smart
+fi
+
 mkdir -p /var/cache/cacti
 
 if ! grep -q /var/cache/cacti /etc/fstab && [ "$HWTYPE" = "physical" ]; then
@@ -27,7 +31,6 @@ if ! grep -q /opt/farm/ext/monitoring-cacti/cron /etc/crontab; then
 	fi
 
 	if [ "$HWTYPE" = "physical" ]; then
-		echo "*/5 * * * * root /opt/farm/ext/monitoring-cacti/cron/smart-helper.sh" >>/etc/crontab
 		echo "1   * * * * root /opt/farm/ext/monitoring-cacti/cron/disklabel-helper.sh" >>/etc/crontab
 	fi
 
@@ -46,4 +49,9 @@ if [ ! -f /root/.ssh/id_cacti ]; then
 
 	echo "key generated, now paste the following public key into `cacti_ssh_target`/.ssh/authorized_keys file:"
 	cat /root/.ssh/id_cacti.pub
+fi
+
+# transitional code: get rid of cron/smart-helper.sh (moved to sf-monitoring-smart extension)
+if grep -q /opt/farm/ext/monitoring-cacti/cron/smart-helper.sh /etc/crontab; then
+	sed -i -e "/\/opt\/farm\/ext\/monitoring-cacti\/cron\/smart-helper.sh/d" /etc/crontab
 fi
